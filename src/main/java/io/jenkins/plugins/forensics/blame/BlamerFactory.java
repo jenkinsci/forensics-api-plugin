e0204cfbfe0b54883908183949c8b4adf98be9f1
Ulli Hafner <ullrich.hafner@gmail.com>
2019-06-21 23:59:10 +0200

    Add some more parameters that are required to create a GitClient.

--- src/main/java/io/jenkins/plugins/forensics/blame/BlamerFactory.java
+++ src/main/java/io/jenkins/plugins/forensics/blame/BlamerFactory.java
@@ -7,9 +7,13 @@ import java.util.stream.Stream;
 import com.google.common.annotations.VisibleForTesting;
 
 import hudson.ExtensionPoint;
+import hudson.FilePath;
+import hudson.model.Run;
+import hudson.model.TaskListener;
 import hudson.scm.SCM;
 
 import io.jenkins.plugins.forensics.blame.Blamer.NullBlamer;
+import io.jenkins.plugins.forensics.util.FilteredLog;
 import io.jenkins.plugins.forensics.util.JenkinsFacade;
 
 /**
@@ -30,30 +34,47 @@ public abstract class BlamerFactory implements ExtensionPoint {
      *
      * @param scm
      *         the {@link SCM} to create the blamer for
+     * @param run
+     *         the current build
+     * @param workspace
+     *         the workspace of the current build
+     * @param listener
+     *         a task listener
+     * @param logger
+     *         a logger to report error messages
      *
      * @return a blamer instance that can blame authors for the specified {@link SCM}
      */
-    public abstract Optional<Blamer> createBlamer(SCM scm);
+    public abstract Optional<Blamer> createBlamer(SCM scm, Run<?, ?> run, FilePath workspace,
+            TaskListener listener, FilteredLog logger);
 
     /**
      * Returns a blamer for the specified {@link SCM}.
      *
      * @param scm
      *         the {@link SCM} to create the blamer for
+     * @param run
+     *         the current build
+     * @param workspace
+     *         the workspace of the current build
+     * @param listener
+     *         a task listener
+     * @param logger
+     *         a logger to report error messages
      *
      * @return a blamer for the specified SCM or a {@link NullBlamer} if no factory is supporting the specified {@link
      *         SCM}
      */
-    public static Blamer findBlamerFor(final SCM scm) {
-        return findAllExtensions(scm).stream()
-                .map(blamerFactory -> blamerFactory.createBlamer(scm))
+    public static Blamer findBlamerFor(final SCM scm, final Run<?, ?> run, final FilePath workspace,
+            final TaskListener listener, final FilteredLog logger) {
+        return findAllExtensions().stream()
+                .map(blamerFactory -> blamerFactory.createBlamer(scm, run, workspace, listener, logger))
                 .flatMap(o -> o.map(Stream::of).orElseGet(Stream::empty))
                 .findFirst()
                 .orElse(new NullBlamer());
     }
 
-    private static List<BlamerFactory> findAllExtensions(final SCM scm) {
+    private static List<BlamerFactory> findAllExtensions() {
         return jenkinsFacade.getExtensionsFor(BlamerFactory.class);
     }
-
 }
--- src/test/java/io/jenkins/plugins/forensics/blame/BlamerFactoryITest.java
+++ src/test/java/io/jenkins/plugins/forensics/blame/BlamerFactoryITest.java
@@ -1,5 +1,6 @@
 package io.jenkins.plugins.forensics.blame;
 
+import java.io.File;
 import java.util.Optional;
 
 import org.junit.ClassRule;
@@ -8,9 +9,13 @@ import org.junit.platform.commons.util.StringUtils;
 import org.jvnet.hudson.test.JenkinsRule;
 import org.jvnet.hudson.test.TestExtension;
 
+import hudson.FilePath;
+import hudson.model.Run;
+import hudson.model.TaskListener;
 import hudson.scm.SCM;
 
 import io.jenkins.plugins.forensics.blame.Blamer.NullBlamer;
+import io.jenkins.plugins.forensics.util.FilteredLog;
 
 import static io.jenkins.plugins.forensics.assertions.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -24,6 +29,7 @@ public class BlamerFactoryITest {
     /** Jenkins rule per suite. */
     @ClassRule
     public static final JenkinsRule JENKINS_PER_SUITE = new JenkinsRule();
+
     private static final String FILE_NAME = "file";
 
     /**
@@ -32,12 +38,17 @@ public class BlamerFactoryITest {
     @Test
     public void shouldReturnSelectedBlamer() {
         SCM scm = mock(SCM.class);
-        Blamer nullBlamer = BlamerFactory.findBlamerFor(scm);
+        Run run = mock(Run.class);
+        File file = mock(File.class);
+        when(file.getPath()).thenReturn("/");
+        FilePath workspace = new FilePath(file);
+        TaskListener listener = TaskListener.NULL;
+        Blamer nullBlamer = BlamerFactory.findBlamerFor(scm, run, workspace, listener, new FilteredLog("Bla"));
         assertThat(nullBlamer).isInstanceOf(NullBlamer.class);
         assertThat(nullBlamer.blame(new FileLocations())).isEmpty();
 
         when(scm.getKey()).thenReturn("git");
-        Blamer testBlamer = BlamerFactory.findBlamerFor(scm);
+        Blamer testBlamer = BlamerFactory.findBlamerFor(scm, run, workspace, listener, new FilteredLog("Bla"));
         assertThat(testBlamer).isInstanceOf(TestBlamer.class);
         assertThat(testBlamer.blame(new FileLocations())).isNotEmpty();
         assertThat(testBlamer.blame(new FileLocations())).hasFiles(FILE_NAME);
@@ -50,7 +61,8 @@ public class BlamerFactoryITest {
     @SuppressWarnings("unused")
     public static class EmptyFactory extends BlamerFactory {
         @Override
-        public Optional<Blamer> createBlamer(final SCM scm) {
+        public Optional<Blamer> createBlamer(final SCM scm, final Run<?, ?> run, final FilePath workspace,
+                final TaskListener listener, final FilteredLog logger) {
             return Optional.empty();
         }
     }
@@ -62,7 +74,8 @@ public class BlamerFactoryITest {
     @SuppressWarnings("unused")
     public static class ActualFactory extends BlamerFactory {
         @Override
-        public Optional<Blamer> createBlamer(final SCM scm) {
+        public Optional<Blamer> createBlamer(final SCM scm, final Run<?, ?> run,
+                final FilePath workspace, final TaskListener listener, final FilteredLog logger) {
             if (StringUtils.isBlank(scm.getKey())) {
                 return Optional.empty();
             }
