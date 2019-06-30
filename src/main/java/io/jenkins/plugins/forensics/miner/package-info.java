9488aae2a21c62345471a61126f63cb5db18f5fa
Ulli Hafner <ullrich.hafner@gmail.com>
2019-07-01 00:06:41 +0200

    Move miner classes to new branch 'git-miner'.

--- src/main/java/io/jenkins/plugins/forensics/miner/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * Provides API classes to obtain commit statistics for files in a repository.
- */
-@DefaultAnnotation(NonNull.class)
-package io.jenkins.plugins.forensics.miner;
-
-import edu.umd.cs.findbugs.annotations.DefaultAnnotation;
-import edu.umd.cs.findbugs.annotations.NonNull;
