d0980cf05f0d7173560be82b473dc7c6d76d0421
Ulli Hafner <ullrich.hafner@gmail.com>
2019-07-01 13:19:28 +0200

    Fix logging of miner.

--- src/main/java/io/jenkins/plugins/forensics/miner/FileStatisticsPublisher.java
+++ src/main/java/io/jenkins/plugins/forensics/miner/FileStatisticsPublisher.java
@@ -55,25 +55,12 @@ public class FileStatisticsPublisher extends Recorder implements SimpleBuildStep
 
         int runtime = (int) (Duration.between(start, end).toMillis() / 1000);
 
-        log(listener, "[Forensics] Analyzed history of %d files in %d seconds", statistics.size(), runtime);
+        log(listener, "Analyzed history of %d files in %d seconds", statistics.size(), runtime);
 
         List<FileStatistics> sorted = new ArrayList<>(statistics.getFileStatistics());
-
-        sorted.sort(Comparator.comparingInt(FileStatistics::getNumberOfCommits).reversed());
-        log(listener, "[Git Forensics] File with most commits (#%d): %s",
-                sorted.get(0).getNumberOfCommits(), sorted.get(0).getFileName());
-
-        sorted.sort(Comparator.comparingInt(FileStatistics::getNumberOfAuthors).reversed());
-        log(listener, "[Git Forensics] File with most number of authors (#%d): %s",
-                sorted.get(0).getNumberOfAuthors(), sorted.get(0).getFileName());
-
-        sorted.sort(Comparator.comparingLong(FileStatistics::getAgeInDays).reversed());
-        log(listener, "[Git Forensics] Oldest file (%d days): %s",
-                sorted.get(0).getAgeInDays(), sorted.get(0).getFileName());
-
-        sorted.sort(Comparator.comparingLong(FileStatistics::getLastModifiedInDays));
-        log(listener, "[Git Forensics] Least recently modified file (%d days): %s",
-                sorted.get(0).getLastModifiedInDays(), sorted.get(0).getFileName());
+        if (!sorted.isEmpty()) {
+            reportResults(listener, sorted);
+        }
 
         run.addAction(new FileStatisticsAction(run, statistics));
 
@@ -82,8 +69,27 @@ public class FileStatisticsPublisher extends Recorder implements SimpleBuildStep
         statistics.getErrorMessages().forEach(listener.getLogger()::println);
     }
 
+    private void reportResults(@Nonnull final TaskListener listener,
+            final List<FileStatistics> sorted) {
+        sorted.sort(Comparator.comparingInt(FileStatistics::getNumberOfCommits).reversed());
+        log(listener, "File with most commits (#%d): %s",
+                sorted.get(0).getNumberOfCommits(), sorted.get(0).getFileName());
+
+        sorted.sort(Comparator.comparingInt(FileStatistics::getNumberOfAuthors).reversed());
+        log(listener, "File with most number of authors (#%d): %s",
+                sorted.get(0).getNumberOfAuthors(), sorted.get(0).getFileName());
+
+        sorted.sort(Comparator.comparingLong(FileStatistics::getAgeInDays).reversed());
+        log(listener, "Oldest file (%d days): %s",
+                sorted.get(0).getAgeInDays(), sorted.get(0).getFileName());
+
+        sorted.sort(Comparator.comparingLong(FileStatistics::getLastModifiedInDays));
+        log(listener, "Least recently modified file (%d days): %s",
+                sorted.get(0).getLastModifiedInDays(), sorted.get(0).getFileName());
+    }
+
     private void log(final TaskListener listener, final String format, final Object... args) {
-        listener.getLogger().println(String.format(format, args));
+        listener.getLogger().println("[Forensics] " + String.format(format, args));
     }
 
     @Override
