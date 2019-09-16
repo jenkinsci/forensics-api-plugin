12db0f1af909e85c628237190bbc43d577ca5ad4
arne <schoenta@hm.edu>
2019-09-16 12:52:17 +0200

    new vcscommit methods

--- src/main/java/io/jenkins/plugins/forensics/reference/BranchMasterIntersectionFinder.java
+++ src/main/java/io/jenkins/plugins/forensics/reference/BranchMasterIntersectionFinder.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.Optional;
 
 /**
- * This class tries to find the reversion of the last shared Commit of the current VCS branch and the master branch.
+ * This class tries to find the revision of the last shared Commit of the current VCS branch and the master branch.
  *
  * @author Arne Schöntag
  */
@@ -15,9 +15,9 @@ public abstract class BranchMasterIntersectionFinder implements RunAction2, Seri
     private static final long serialVersionUID = -4549516129641755356L;
 
     /**
-     * Method to determine the Reversion of the last Commit which is shared with the master branch.
+     * Method to determine the Revision of the last Commit which is shared with the master branch.
      *
-     * @return the hash value (ObjectId) of the reversion
+     * @return the hash value (ObjectId) of the revision
      *          or null if an error occurred during evaluation or no intersection was found (should not happen)
      */
     public abstract Optional<String> findReferencePoint();
--- src/main/java/io/jenkins/plugins/forensics/reference/VCSCommit.java
+++ src/main/java/io/jenkins/plugins/forensics/reference/VCSCommit.java
@@ -4,6 +4,7 @@ import jenkins.model.RunAction2;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Represents an Item in the List of Commits which will be used to find an Intersection with another job.
@@ -15,4 +16,20 @@ public interface VCSCommit extends RunAction2, Serializable {
     void addGitCommitLogs(List<String> revisions);
 
     String getSummary();
+
+    String getLatestRevision();
+
+    List<String> getRevisions();
+
+    void addRevisions(List<String> list);
+
+    void addRevision(String revision);
+
+    /**
+     * Tries to find the reference point of the GitCommit of another build.
+     * @param reference the GitCommit of the other build
+     * @param maxLogs maximal amount of commits looked at.
+     * @return the build Id of the reference build or Optional.empty() if none found.
+     */
+    Optional<String> getReferencePoint(VCSCommit reference, int maxLogs);
 }
