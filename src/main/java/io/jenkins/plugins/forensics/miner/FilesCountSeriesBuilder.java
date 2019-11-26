dabd425522f124981f32df56736bead31972c8fd
Ulli Hafner <ullrich.hafner@gmail.com>
2019-11-26 09:55:37 +0100

    Bump echarts to latest snapshot.

--- src/main/java/io/jenkins/plugins/forensics/miner/FilesCountSeriesBuilder.java
+++ src/main/java/io/jenkins/plugins/forensics/miner/FilesCountSeriesBuilder.java
@@ -3,7 +3,6 @@ package io.jenkins.plugins.forensics.miner;
 import java.util.HashMap;
 import java.util.Map;
 
-import io.jenkins.plugins.echarts.api.charts.BuildResult;
 import io.jenkins.plugins.echarts.api.charts.SeriesBuilder;
 
 /**
@@ -15,9 +14,9 @@ public class FilesCountSeriesBuilder extends SeriesBuilder<RepositoryStatistics>
     static final String TOTALS_KEY = "total";
 
     @Override
-    protected Map<String, Integer> computeSeries(final BuildResult<RepositoryStatistics> current) {
+    protected Map<String, Integer> computeSeries(final RepositoryStatistics current) {
         Map<String, Integer> series = new HashMap<>();
-        series.put(TOTALS_KEY, current.getResult().size());
+        series.put(TOTALS_KEY, current.size());
         return series;
     }
 }
