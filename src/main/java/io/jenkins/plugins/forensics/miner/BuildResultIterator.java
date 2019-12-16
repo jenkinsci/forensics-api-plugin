66510f1c8006b4e9a181bb1c99520489b098960a
Ulli Hafner <ullrich.hafner@gmail.com>
2019-12-16 09:59:02 +0100

    Add JavaDoc.

--- src/main/java/io/jenkins/plugins/forensics/miner/BuildResultIterator.java
+++ src/main/java/io/jenkins/plugins/forensics/miner/BuildResultIterator.java
@@ -10,15 +10,35 @@ import io.jenkins.plugins.echarts.Build;
 import io.jenkins.plugins.echarts.BuildResult;
 import io.jenkins.plugins.util.BuildAction;
 
+/**
+ * Iterates over a collection of builds that contain results of a given generic type. A new iterator starts from a
+ * baseline build where it selects the attached action of the given type. From this action it obtains and returns the
+ * current result. Then it moves back in the build history until no more builds are available.
+ *
+ * @param <A>
+ *         type of the action that stores the result
+ * @param <R>
+ *         type of the result that is persisted within the action
+ *
+ * @author Ullrich Hafner
+ */
 @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
 public class BuildResultIterator<A extends BuildAction<R>, R> implements Iterator<BuildResult<R>> {
     private final Class<A> actionType;
 
     private Optional<A> latestAction;
 
-    public BuildResultIterator(final Class<A> actionType, final Optional<A> latestAction) {
+    /**
+     * Creates a new iterator that selects action of the given type {@code actionType}.
+     *
+     * @param actionType
+     *         the type of the actions to select
+     * @param baseline
+     *         the baseline to start from
+     */
+    public BuildResultIterator(final Class<A> actionType, final Optional<A> baseline) {
         this.actionType = actionType;
-        this.latestAction = latestAction;
+        this.latestAction = baseline;
     }
 
     @Override
@@ -29,12 +49,15 @@ public class BuildResultIterator<A extends BuildAction<R>, R> implements Iterato
     @Override
     public BuildResult<R> next() {
         if (!latestAction.isPresent()) {
-            throw new NoSuchElementException();
+            throw new NoSuchElementException("There is no action available anymore. Use hasNext() before calling next().");
         }
+
         A buildAction = latestAction.get();
         Run<?, ?> run = buildAction.getOwner();
         latestAction = BuildAction.getBuildActionFromHistoryStartingFrom(run.getPreviousBuild(), actionType);
 
-        return new BuildResult<>(new Build(run.getNumber(), run.getDisplayName(), (int)(run.getTimeInMillis() / 1000)), buildAction.getResult());
+        int buildTimeInSeconds = (int) (run.getTimeInMillis() / 1000);
+        Build build = new Build(run.getNumber(), run.getDisplayName(), buildTimeInSeconds);
+        return new BuildResult<>(build, buildAction.getResult());
     }
 }
