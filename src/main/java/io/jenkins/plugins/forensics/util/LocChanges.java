cdb8b7e01f886f096afddbd38164a0434ba2e500
rathaya <giulia.delbravo@gmx.de>
2020-09-09 02:54:43 -0700

    Made changes to LocChanges class.

--- src/main/java/io/jenkins/plugins/forensics/util/LocChanges.java
+++ src/main/java/io/jenkins/plugins/forensics/util/LocChanges.java
@@ -9,36 +9,29 @@ public class LocChanges {
 
     private int totalLoc;
     private final String commitId;
-    private int addedLines;
+    private int totalAddedLines;
     private int deletedLines;
 
-        public LocChanges(String commitId){
+        public LocChanges(final String commitId){
             this.commitId = commitId;
             totalLoc = 0;
-            addedLines = 0;
+            totalAddedLines = 0;
             deletedLines = 0;
         }
 
-        public void addTotalLoc(int newLines) {
-            totalLoc += newLines;
-        }
-
-        public void addAddedLines(int addedLines) {
-            this.addedLines += addedLines;
-            totalLoc+= addedLines;
-        }
-
-        public void addDeletedLines(int removedLines) {
+        public void updateLocChanges(final int addedLines, final int removedLines) {
+            totalLoc += addedLines;
+            totalLoc -= removedLines;
+            this.totalAddedLines += addedLines;
             this.deletedLines += removedLines;
-            totalLoc-=removedLines;
         }
 
         public int getTotalLoc() {
             return totalLoc;
         }
 
-        public int getAddedLines(){
-            return addedLines;
+        public int getTotalAddedLines(){
+            return totalAddedLines;
         }
 
         public int getDeletedLines() {
