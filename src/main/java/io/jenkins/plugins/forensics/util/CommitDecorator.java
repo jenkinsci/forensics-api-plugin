b8db469e46fd364f8786d82b4df73a6338d443c1
Ulli Hafner <ullrich.hafner@gmail.com>
2020-10-06 14:30:54 +0200

    Add a class to generate HTML links to repository commits.

---
+++ src/main/java/io/jenkins/plugins/forensics/util/CommitDecorator.java
@@ -0,0 +1,32 @@
+package io.jenkins.plugins.forensics.util;
+
+import hudson.scm.RepositoryBrowser;
+
+/**
+ * A {@link RepositoryBrowser} for commits. Since a {@link RepositoryBrowser} has no API to generate links to
+ * simple commits, this decorator adds such a functionality. Note that this API does not only obtain
+ * such links, it also renders these links as HTML a tags.
+ *
+ * @author Ullrich Hafner
+ */
+public abstract class CommitDecorator {
+    /**
+     * Obtains a link for the specified commit ID.
+     *
+     * @param id
+     *         the ID of the commit
+     *
+     * @return an HTML a tag that
+     */
+    public abstract String asLink(String id);
+
+    /**
+     * A decorator that does nothing.
+     */
+    public static class NullDecorator extends CommitDecorator {
+        @Override
+        public String asLink(final String id) {
+            return id;
+        }
+    }
+}
