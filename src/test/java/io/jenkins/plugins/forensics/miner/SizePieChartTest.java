1572a4c16068f2b45ba5c0caa891958730089123
bleibdirtroy <adrian.germeck@hm.edu>
2021-10-21 10:11:58 +0200

    added basic test for `SizePieChart`

---
+++ src/test/java/io/jenkins/plugins/forensics/miner/SizePieChartTest.java
@@ -0,0 +1,32 @@
+package io.jenkins.plugins.forensics.miner;
+
+import java.util.function.Function;
+
+import org.junit.jupiter.api.Test;
+
+import edu.hm.hafner.echarts.PieChartModel;
+
+
+import io.jenkins.plugins.forensics.miner.FileStatistics.FileStatisticsBuilder;
+
+class SizePieChartTest {
+
+
+    @Test
+    void shouldCreate() {
+        SizePieChart chart = new SizePieChart();
+
+        RepositoryStatistics repositoryStatistics = new RepositoryStatistics();
+        FileStatisticsBuilder fileStatisticsBuilder = new FileStatistics.FileStatisticsBuilder();
+        FileStatistics fileStatistics = fileStatisticsBuilder.build("Test");
+        repositoryStatistics.add(fileStatistics);
+        Function<FileStatistics, Integer> sizeMethod = fileStatistics1 -> 5;
+        int breakpoint = 3;
+
+
+        PieChartModel model = chart.create(repositoryStatistics,sizeMethod,breakpoint);
+
+
+
+    }
+}
