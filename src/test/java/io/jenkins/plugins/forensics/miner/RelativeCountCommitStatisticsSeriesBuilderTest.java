830aaa49f5a396b94cf24d2f5078ea20a92cfd28
bleibdirtroy <adrian.germeck@hm.edu>
2021-10-27 17:24:17 +0200

    added mock

--- src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountCommitStatisticsSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountCommitStatisticsSeriesBuilderTest.java
@@ -3,10 +3,17 @@ package io.jenkins.plugins.forensics.miner;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class RelativeCountCommitStatisticsSeriesBuilderTest {
 
     @Test
     void computeSeries() {
+        RelativeCountCommitStatisticsSeriesBuilder relativeCountCommitStatisticsSeriesBuilder = new RelativeCountCommitStatisticsSeriesBuilder();
+        CommitStatisticsBuildAction commitStatisticsBuildActionStub = mock(CommitStatisticsBuildAction.class);
+        when(commitStatisticsBuildActionStub.getCommitStatistics()).thenReturn(new CommitStatistics());
+
+        relativeCountCommitStatisticsSeriesBuilder.computeSeries(commitStatisticsBuildActionStub);
+
     }
 }
