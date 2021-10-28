abe23481a28ca74d465cdb644b4799cf3c25b696
Katharina Winkler <Katharina.Winkler01@sap.com>
2021-10-28 11:42:26 +0200

    Tests the CodeMetricSeriesBuilder

---
+++ src/test/java/io/jenkins/plugins/forensics/miner/CodeMetricSeriesBuilderTest.java
@@ -0,0 +1,27 @@
+package io.jenkins.plugins.forensics.miner;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class CodeMetricSeriesBuilderTest {
+
+    @Test
+    void computeSeries() {
+
+        ForensicsBuildAction action = mock(ForensicsBuildAction.class);
+        when(action.getTotalLinesOfCode()).thenReturn(2);//
+        when(action.getTotalChurn()).thenReturn(5);
+        CodeMetricSeriesBuilder builder = new CodeMetricSeriesBuilder();
+        Map<String, Integer> series = builder.computeSeries(action);
+        assertThat(series.get("loc")).isEqualTo(2);
+        assertThat(series.get("churn")).isEqualTo(5);
+       
+    }
+
+    //Bonus: Assert ob die Klassen die Methoden haben
+
+} 
