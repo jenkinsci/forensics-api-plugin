5c997af050be0524b5cf7c3e57d626e262aa8e8c
Adrian Germeck <adriangermeck7@gmail.com>
2021-11-02 18:05:45 +0000

    added constants and stub method

--- src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountCommitStatisticsSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountCommitStatisticsSeriesBuilderTest.java
@@ -8,22 +8,37 @@ import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 class RelativeCountCommitStatisticsSeriesBuilderTest {
+    static final String COMMITS = "commits";
+    static final String AUTHORS = "authors";
+    static final String FILES = "files";
 
     @Test
     void computeSeries() {
-        RelativeCountCommitStatisticsSeriesBuilder relativeCountCommitStatisticsSeriesBuilder = new RelativeCountCommitStatisticsSeriesBuilder();
-        CommitStatisticsBuildAction commitStatisticsBuildActionStub = mock(CommitStatisticsBuildAction.class);
-        CommitStatistics commitStatistics = mock(CommitStatistics.class);
-        when(commitStatisticsBuildActionStub.getCommitStatistics()).thenReturn(commitStatistics);
-        when(commitStatistics.getCommitCount()).thenReturn(7);
-        when(commitStatistics.getAuthorCount()).thenReturn(11);
-        when(commitStatistics.getFilesCount()).thenReturn(17);
+        final int commitCount = 7;
+        final int authorCount = 7;
+        final int filesCount = 7;
 
+        RelativeCountCommitStatisticsSeriesBuilder relativeCountCommitStatisticsSeriesBuilder = new RelativeCountCommitStatisticsSeriesBuilder();
+        CommitStatisticsBuildAction commitStatisticsBuildActionStub = getCommitStatisticsBuildActionStub(
+                commitCount, authorCount, filesCount);
         Map<String, Integer> result = relativeCountCommitStatisticsSeriesBuilder.computeSeries(
                 commitStatisticsBuildActionStub);
 
-        assertThat(result.get("commits")).isEqualTo(7);
-        assertThat(result.get("authors")).isEqualTo(11);
-        assertThat(result.get("files")).isEqualTo(17);
+        assertThat(result)
+                .containsEntry(COMMITS, commitCount)
+                .containsEntry(AUTHORS, authorCount)
+                .containsEntry(FILES, filesCount);
+    }
+
+    private CommitStatisticsBuildAction getCommitStatisticsBuildActionStub(final int commitCount, final int authorCount,
+            final int filesCount) {
+        CommitStatisticsBuildAction commitStatisticsBuildActionStub = mock(CommitStatisticsBuildAction.class);
+        CommitStatistics commitStatistics = mock(CommitStatistics.class);
+        when(commitStatisticsBuildActionStub.getCommitStatistics()).thenReturn(commitStatistics);
+        when(commitStatistics.getCommitCount()).thenReturn(commitCount);
+        when(commitStatistics.getAuthorCount()).thenReturn(authorCount);
+        when(commitStatistics.getFilesCount()).thenReturn(filesCount);
+        return commitStatisticsBuildActionStub;
     }
 }
+
