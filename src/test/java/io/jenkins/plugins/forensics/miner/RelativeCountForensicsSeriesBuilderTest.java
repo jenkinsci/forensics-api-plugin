3ff8a898cd156bb21a9d458eb5909ce60872df9a
Katharina Winkler <44469598+kathwin@users.noreply.github.com>
2021-11-02 16:11:57 +0100

    Update src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountForensicsSeriesBuilderTest.java
    Co-authored-by: Ullrich Hafner <ullrich.hafner@gmail.com>

--- src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountForensicsSeriesBuilderTest.java
+++ src/test/java/io/jenkins/plugins/forensics/miner/RelativeCountForensicsSeriesBuilderTest.java
@@ -9,8 +9,6 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
-
-
 class RelativeCountForensicsSeriesBuilderTest {
 
     @Test
