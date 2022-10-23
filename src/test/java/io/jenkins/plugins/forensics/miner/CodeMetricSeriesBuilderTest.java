56a60f08def200bc4b548dd70d3c611203e408f3
lemorse <arthurr.caron@gmail.com>
2022-10-23 10:28:52 +0200

    [JENKINS-66861] - CodeMetricSeriesBuilder test added.

---
+++ src/test/java/io/jenkins/plugins/forensics/miner/CodeMetricSeriesBuilderTest.java
@@ -0,0 +1,30 @@
+package io.jenkins.plugins.forensics.miner;
+
+        import static org.mockito.Mockito.mock;
+        import static org.mockito.Mockito.when;
+
+        import java.util.Map;
+
+        import org.junit.jupiter.api.Test;
+
+        import static org.assertj.core.api.Assertions.assertThat;
+
+class CodeMetricSeriesBuilderTest {
+
+    @Test
+    void shouldComputeRelativeCountStatistics() {
+        final int totalLinesOfCode = 10;
+        final int totalChurn = 20;
+        ForensicsBuildAction forensicsBuildAction = mock(ForensicsBuildAction.class);
+        when(forensicsBuildAction.getTotalLinesOfCode()).thenReturn(totalLinesOfCode);
+        when(forensicsBuildAction.getTotalChurn()).thenReturn(totalChurn);
+
+        CodeMetricSeriesBuilder codeMetricSeriesBuilder = new CodeMetricSeriesBuilder();
+        Map<String,Integer> computedSeries = codeMetricSeriesBuilder.computeSeries(forensicsBuildAction);
+
+        assertThat(computedSeries).hasSize(2);
+        assertThat(computedSeries.get(CodeMetricSeriesBuilder.LOC_KEY)).isEqualTo(totalLinesOfCode);
+        assertThat(computedSeries.get(CodeMetricSeriesBuilder.CHURN_KEY)).isEqualTo(totalChurn);
+    }
+
+}
